234b6dd82b97d447134b232f0bed6addd90f12c1
Vraj Patel <39704588+ChocoMufn@users.noreply.github.com>
2025-03-11 21:40:34 -0400

    Arduino code changes

--- src/arduino/main.ino
+++ src/arduino/main.ino
@@ -1,19 +1,13 @@
-// Arduino Mecanum Drive + Stepper Control
+const int rightMotorPins[4] = {2, 3, 4, 5};
+const int leftMotorPins[4] = {11, 10, 12, 13};
+const int motorEnable = 6;
 
-// Motor Control Pins (DC Motors)
-const int rightMotorPins[4] = {2, 3, 4, 5}; // Right side L298N
-const int leftMotorPins[4] = {11, 10, 12, 13}; // Left side L298N
-const int motorEnable = 6; // Enable pin for all DC motors
-
-// Stepper Motor Pins
 const int stepperIn3 = 7;
 const int stepperIn4 = 8;
 
-// Encoder Pins (Not used in this simple implementation, but defined)
 const int encoderA = A0;
 const int encoderB = A5;
 
-// Limit Switch Pin
 const int limitSwitch = 9;
 
 bool liftDown = false;
@@ -21,21 +15,17 @@ bool liftDown = false;
 void setup() {
     Serial.begin(9600);
     
-    // Set motor control pins as OUTPUT
     for (int i = 0; i < 4; i++) {
         pinMode(rightMotorPins[i], OUTPUT);
         pinMode(leftMotorPins[i], OUTPUT);
     }
     pinMode(motorEnable, OUTPUT);
     
-    // Stepper motor pins
     pinMode(stepperIn3, OUTPUT);
     pinMode(stepperIn4, OUTPUT);
     
-    // Limit switch
     pinMode(limitSwitch, INPUT_PULLUP);
     
-    // Enable motors
     digitalWrite(motorEnable, HIGH);
 }
 
@@ -48,7 +38,7 @@ void moveLift(int duration) {
 }
 
 void moveRobot(int speed, int r1, int r2, int l1, int l2) {
-    analogWrite(motorEnable, speed);
+    analogWrite(motorEnable, abs(speed));
     digitalWrite(rightMotorPins[0], r1);
     digitalWrite(rightMotorPins[1], r2);
     digitalWrite(rightMotorPins[2], r1);
@@ -77,33 +67,53 @@ void loop() {
             digitalWrite(stepperIn4, LOW);
             liftDown = true;
         } else {
-            int speed = 0;
-            int spaceIndex = command.indexOf(' ');
+            int spaceIndex = command.indexOf(':');
             if (spaceIndex != -1) {
-                speed = command.substring(spaceIndex + 1).toInt();
-                command = command.substring(0, spaceIndex);
-            }
-
-            if (command == "fw") {
-                moveRobot(speed, HIGH, LOW, HIGH, LOW);
-            } else if (command == "bw") {
-                moveRobot(speed, LOW, HIGH, LOW, HIGH);
-            } else if (command == "rs") {
-                moveRobot(speed, LOW, HIGH, HIGH, LOW);
-            } else if (command == "ls") {
-                moveRobot(speed, HIGH, LOW, LOW, HIGH);
-            } else if (command == "flip" || command == "left") {
-                moveRobot(speed, LOW, HIGH, HIGH, LOW);
-                delay(2000); // Adjust for 180-degree turn based on speed
-                moveRobot(0, LOW, LOW, LOW, LOW);
-            } else if (command == "right") {
-                moveRobot(speed, HIGH, LOW, LOW, HIGH);
-                delay(2000); // Adjust for 180-degree turn based on speed
-                moveRobot(0, LOW, LOW, LOW, LOW);
-            } else {
-                moveRobot(0, LOW, LOW, LOW, LOW);
-                digitalWrite(stepperIn3, LOW);
-                digitalWrite(stepperIn4, LOW);
+                String cmdType = command.substring(0, spaceIndex);
+                int speed = command.substring(spaceIndex + 1).toInt();
+                
+                if (cmdType == "forwards") {
+                    if (speed > 0) {
+                        moveRobot(speed, HIGH, LOW, HIGH, LOW);
+                    } else {
+                        moveRobot(-speed, LOW, HIGH, LOW, HIGH);
+                    }
+                } else if (cmdType == "turn") {
+                    if (speed > 0) {
+                        moveRobot(speed, HIGH, LOW, LOW, HIGH);
+                        delay(2000);
+                        moveRobot(0, LOW, LOW, LOW, LOW);
+                    } else {
+                        moveRobot(-speed, LOW, HIGH, HIGH, LOW);
+                        delay(2000);
+                        moveRobot(0, LOW, LOW, LOW, LOW);
+                    }
+                } else if (cmdType == "side") {
+                    analogWrite(motorEnable, abs(speed));
+                    if (speed > 0) {
+                        digitalWrite(rightMotorPins[0], LOW);
+                        digitalWrite(rightMotorPins[1], HIGH);
+                        digitalWrite(rightMotorPins[2], HIGH);
+                        digitalWrite(rightMotorPins[3], LOW);
+                        digitalWrite(leftMotorPins[0], LOW);
+                        digitalWrite(leftMotorPins[1], HIGH);
+                        digitalWrite(leftMotorPins[2], HIGH);
+                        digitalWrite(leftMotorPins[3], LOW);
+                    } else {
+                        digitalWrite(rightMotorPins[0], HIGH);
+                        digitalWrite(rightMotorPins[1], LOW);
+                        digitalWrite(rightMotorPins[2], LOW);
+                        digitalWrite(rightMotorPins[3], HIGH);
+                        digitalWrite(leftMotorPins[0], HIGH);
+                        digitalWrite(leftMotorPins[1], LOW);
+                        digitalWrite(leftMotorPins[2], LOW);
+                        digitalWrite(leftMotorPins[3], HIGH);
+                    }
+                } else {
+                    moveRobot(0, LOW, LOW, LOW, LOW);
+                    digitalWrite(stepperIn3, LOW);
+                    digitalWrite(stepperIn4, LOW);
+                }
             }
         }
     }
